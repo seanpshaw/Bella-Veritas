e699b9019d02c7fa5cc275c1dc52749c4a9838e3
Sean Shaw <seanpshaw@gmail.com>
2011-04-24 21:20:54 -0500

    updated theme

--- sites/all/themes/tenseconds/templates/page.tpl.php
+++ sites/all/themes/tenseconds/templates/page.tpl.php
@@ -121,10 +121,14 @@
           </div> <!-- /#footer-inner -->
 		    </div> <!-- /#footer -->
 		    <div id="footer-bottom" class="container-16 clearfix clear">
-		      <div id="address_raw"><?php print $address_raw; ?></div>
-		      <?php print $link; ?>
-		      <div id="email"><a href="mailto:info@tensecondsmovie.com" title="10 Seconds Email" rel="">info@10secondsmovie.com</a></div>
 		      <div id="bella-logo"><a href="http://www.bellaveritas.com" title="Bella Veritas Productions" rel="Bella Veritas Productions"><img src="sites/all/themes/tenseconds/images/bella-logo.png" alt="Bella Veritas Productions" /></a></div>
+		      <div class="grid-8 footer-bottom-left">
+		        <div id="address_raw"><?php print $address_raw; ?></div>
+		      </div>
+		      <div class="grid-8 footer-bottom-right">
+  		      <?php print $link; ?>
+  		      <div id="email"><a href="mailto:info@tensecondsmovie.com" title="10 Seconds Email" rel="">info@10secondsmovie.com</a></div>
+  		    </div>
 		    </div>
 		  </div> <!-- /#footer-wrapper -->
 	  <?php endif; ?>
