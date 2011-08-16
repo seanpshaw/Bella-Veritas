cb44cd4601987998fd0547fdc6ed6e1186154355
Sean Shaw <seanpshaw@gmail.com>
2011-08-15 22:41:02 -0500

    10 seconds: theme cleanup

--- sites/all/themes/tenseconds/js/script.js
+++ sites/all/themes/tenseconds/js/script.js
@@ -1,7 +1,3 @@
-$(document).ready(function () {
-    $('li.menu-118 > a').contents().unwrap();
-});
-
 Drupal.behaviors.initColorbox = function (context) {
   var settings = Drupal.settings.colorbox;
   settings.slideshow = true;
