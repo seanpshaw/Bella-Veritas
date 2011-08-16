8e386e715be8c9bb9f19bda155a30d25b3550e90
Sean Shaw <seanpshaw@gmail.com>
2011-08-16 01:48:39 -0500

    10 seconds: theme cleanup

--- sites/all/themes/tenseconds/js/script.js
+++ sites/all/themes/tenseconds/js/script.js
@@ -1,10 +1,1 @@
-Drupal.behaviors.initColorbox = function (context) {
-  var settings = Drupal.settings.colorbox;
-  settings.slideshow = true;
-  $('a, area, input', context)
-    .filter('.colorbox-slideshow:not(.initColorbox-processed)')
-    .addClass('initColorbox-processed')
-    .colorbox(settings);
-
-}
 
